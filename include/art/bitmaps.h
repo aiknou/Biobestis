6bfbde9849e94b19bc5fc936b31e727370b34f67
asika <asikainenaino@gmail.com>
2026-03-12 16:40:28 +0200

    added some reactions

--- include/art/bitmaps.h
+++ include/art/bitmaps.h
@@ -5,17 +5,17 @@
 #define EYE_H 32
 
 #include "heartEyes.h"    // level 0 - Great!
-#include "starEyes.h"     // level 1 - Good
-#include "sleepyEyes.h"   // level 2 - OK
-#include "hoodedEyes.h"   // level 3 - Meh
-#include "sadEyes.h"      // level 4 - Bad
+#include "happyEyes.h"     // level 1 - Good
+#include "hoodedEyes.h"   // level 2 - OK
+#include "sadEyes.h"   // level 3 - Meh
+#include "cryingEyes.h"      // level 4 - Bad
 #include "angryEyes.h"    // level 5 - Critical!
 
 static const unsigned char* const statusBitmaps[6] PROGMEM = {
     heartEyesBitmap,
-    starEyesBitmap,
-    sleepyEyesBitmap,
+    happyEyesBitmap,
     hoodedEyesBitmap,
     sadEyesBitmap,
+    cryingEyesBitmap,
     angryEyesBitmap
 };
